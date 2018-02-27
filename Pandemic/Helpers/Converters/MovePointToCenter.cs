3c3301b4755e006ac3faeaf5c99345aae341eaa9
Jan Skácel <skaceljan@seznam.cz>
2018-02-27 16:48:50 +0100

    Added few connections

---
+++ Pandemic/Helpers/Converters/MovePointToCenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Pandemic.Converters
+{
+    [ValueConversion(typeof(double), typeof(double))]
+    public class MovePointToCenter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value + (double)parameter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value - (double)parameter;
+        }
+    }
+}
